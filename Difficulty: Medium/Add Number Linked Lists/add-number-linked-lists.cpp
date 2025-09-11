983b1eac33ae890a98b78b5d97d2b76e48548db8
Akanksha Lavale <akankshalavale06@gmail.com>
2025-09-11 11:22:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add Number Linked Lists/add-number-linked-lists.cpp
@@ -0,0 +1,71 @@
+class Solution {
+  public:
+
+    Node* Reverse(Node* curr, Node* prev) {
+        if (curr == NULL)
+            return prev;
+        Node* front = curr->next;
+        curr->next = prev;
+        return Reverse(front, curr);
+    }
+
+    Node* removeLeadingZeros(Node* head) {
+        while (head && head->data == 0) {
+            head = head->next;
+        }
+        return head ? head : new Node(0); 
+    }
+
+    Node* addTwoLists(Node* head1, Node* head2) {
+        
+        head1 = Reverse(head1, NULL);
+        head2 = Reverse(head2, NULL);
+
+        Node* curr1 = head1;
+        Node* curr2 = head2;
+
+        Node* dummy = new Node(0);
+        Node* tail = dummy;
+        int carry = 0;
+
+        while (curr1 && curr2) {
+            int sum = curr1->data + curr2->data + carry;
+            tail->next = new Node(sum % 10);
+            carry = sum / 10;
+
+            tail = tail->next;
+            curr1 = curr1->next;
+            curr2 = curr2->next;
+        }
+
+        while (curr1) {
+            int sum = curr1->data + carry;
+            tail->next = new Node(sum % 10);
+            carry = sum / 10;
+
+            tail = tail->next;
+            curr1 = curr1->next;
+        }
+
+        while (curr2) {
+            int sum = curr2->data + carry;
+            tail->next = new Node(sum % 10);
+            carry = sum / 10;
+
+            tail = tail->next;
+            curr2 = curr2->next;
+        }
+
+        while (carry) {
+            tail->next = new Node(carry % 10);
+            carry /= 10;
+            tail = tail->next;
+        }
+
+    
+        Node* result = Reverse(dummy->next, NULL);
+        result = removeLeadingZeros(result);
+
+        return result;
+    }
+};
