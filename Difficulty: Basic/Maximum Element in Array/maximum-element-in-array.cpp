c16a9ee76e5cd6d9c577a7afc9c128373d5741d0
Akanksha Lavale <akankshalavale06@gmail.com>
2025-10-07 23:35:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum Element in Array/maximum-element-in-array.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int largest(int arr[], int n) {
+      int maxElement = arr[0]; 
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > maxElement) {
+            maxElement = arr[i];  
+        }
+    }
+
+    return maxElement;
+    }
+};
