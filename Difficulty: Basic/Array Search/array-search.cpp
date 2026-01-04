2e37ad1330f0ee73262f5bf8a00d877c5f041262
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-04 16:46:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array Search/array-search.cpp
+++ Difficulty: Basic/Array Search/array-search.cpp
@@ -1,20 +1,14 @@
 class Solution {
   public:
-      
-   
-    int linearsearch(vector<int>& arr, int x, int index, int n) {
-        if(index == n){
-            return -1;
-        }
-        if(arr[index] == x){
-            return index;
-        }
-        return linearsearch(arr, x, index + 1, n);
-    }
-
- 
     int search(vector<int>& arr, int x) {
-        int n = arr.size();
-        return linearsearch(arr, x, 0, n);
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        
+        return -1;
     }
 };
