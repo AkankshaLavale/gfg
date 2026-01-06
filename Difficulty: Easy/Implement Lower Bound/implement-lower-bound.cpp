43c070f94b0ec4b2aff4e90d08c7fa922ea26033
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-07 02:26:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Implement Lower Bound/implement-lower-bound.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int lowerBound(vector<int>& arr, int x) {
+    int start = 0;
+    int end = arr.size() - 1;
+    int ans = arr.size();   
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+
+        if (arr[mid] >= x) {
+            ans = mid;     
+            end = mid - 1; 
+        } else {
+            start = mid + 1; 
+        }
+    }
+    return ans;
+    }
+
+};
