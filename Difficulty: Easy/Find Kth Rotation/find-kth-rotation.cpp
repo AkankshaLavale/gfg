360f2f2f756bf79aca45725d59f25efb7d9c1196
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-09 23:18:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Kth Rotation/find-kth-rotation.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int findKRotation(vector<int> &arr) {
+        // Code Here
+        int start = 0;
+        int end = arr.size()-1;
+        
+        while(start<end){
+            int mid = start+(end-end)/2;
+            if(arr[mid]>arr[end]){
+                start = mid +1;
+            }else{
+                end = mid ;
+            }
+        }
+        
+        return start ;
+    }
+};
