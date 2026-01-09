cee87bb7f3f681aa7ea1eb6f28155f108b5ef3e1
Akanksha Lavale <akankshalavale06@gmail.com>
2026-01-10 01:30:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,16 +1,53 @@
 class Solution {
   public:
-    int countFreq(vector<int>& arr, int target) {
-        // code here
-        int count  = 0;
-        for(int i=0;i<arr.size();i++){
-            
-            if(arr[i]==target){
-                count = count+1;
+    
+    int firstOcc(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;         
+                end = mid - 1;      
             }
-            
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
         }
-       
-       return count; 
+        return ans;
+    }
+
+    int lastOcc(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1;
+        int ans = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                ans = mid;         
+                start = mid + 1;    
+            }
+            else if (arr[mid] < target) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    int countFreq(vector<int>& arr, int target) {
+        int first = firstOcc(arr, target);
+        if (first == -1) return 0;  
+
+        int last = lastOcc(arr, target);
+        return last - first + 1;
     }
 };
