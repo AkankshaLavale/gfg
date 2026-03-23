28fbdf6cf88a01b976ad199a9d1e19c91289c2c8
Akanksha Lavale <akankshalavale06@gmail.com>
2026-03-23 11:40:20 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.cpp
+++ Difficulty: Easy/Make the array beautiful/make-the-array-beautiful.cpp
@@ -1,34 +1,19 @@
 class Solution {
   public:
     vector<int> makeBeautiful(vector<int> arr) {
-        int n = arr.size();
-        stack<int> st;
-
-        for (int i = 0; i < n; i++) {
-            if (st.empty()) {
-                st.push(arr[i]);
-            } else if (arr[i] >= 0) {
-                if (st.top() >= 0) {
-                    st.push(arr[i]);
-                } else {
-                    st.pop();
-                }
-            } else {
-                if (st.top() < 0) {
-                    st.push(arr[i]);
-                } else {
-                    st.pop();
-                }
+        
+        vector<int> ans;
+        
+        for(int i = 0; i < arr.size(); i++){
+            if(!ans.empty() && 
+               ((ans.back() >= 0 && arr[i] < 0) || (ans.back() < 0 && arr[i] >= 0))){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(arr[i]);
             }
         }
-
-        vector<int> ans(st.size());
-        int i = st.size() - 1;
-        while (!st.empty()) {
-            ans[i--] = st.top();
-            st.pop();
-        }
-
+        
         return ans;
     }
 };
